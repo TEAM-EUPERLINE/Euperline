1cb3047328ac220a694f9ec908cd8e78bd12bfd0
Talha Akyuzlu <64819535+X3RESS@users.noreply.github.com>
2020-07-30 14:22:16 +0300

    Add files via upload

---
+++ re4p_paket.sh
@@ -0,0 +1,54 @@
+clear
+  clear
+
+echo "                    ."
+echo "                   ..."
+echo "                  ....."
+echo "                 ......."
+echo "                ........."
+echo "                 ......."
+echo "          .       .....       ."
+echo "         ...       ...       ..."
+echo "        .....       .       ....."
+echo "       .......             ......."
+echo "      .........           ........."
+echo "     .........             ........."
+echo -e "    .........       \e[34m.\e[0m       ........."
+echo -e "   .........       \e[34m...\e[0m       ........."
+echo -e "  .........       \e[34m.....\e[0m       ........."
+echo -e " .........       \e[34m.......\e[0m       ........."
+echo -e ".........       \e[34m.........\e[0m       ........."
+echo -e "\e[34m-----------------------------------------\e[0m"
+echo -e "-\e[40;38;5;82m Euperline.com \e[30;48;5;82m RE4P-TEAM-CORE \e[0m -"
+echo -e "\e[34m-----------------------------------------\e[0m"
+echo -e "\e[5;34mTEAM RE4P CORE STANDART\e[0m"
+echo -e "\e[34m-----------------------------------------\e[0m"
+echo -e "\e[34m-----------< PAKET KURULUMU >------------\e[0m"
+
+sudo apt install -y figlet
+sudo apt install -y curl
+sudo apt install -y nmap
+sudo apt install -y toilet
+s
+udo apt install python3-pip
+sudo apt install python-pip
+
+sudo pip3 install selenium
+sudo pip3 install pydub
+sudo pip3 install speechrecognition
+sudo pip3 install termcolor
+sudo pip3 install ns4
+sudo pip3 install bs4
+sudo pip3 install opensimplex
+sudo pip3 install pyqtgraph
+sudo pip3 install pyopengl
+sudo pip3 install qtopengl
+sudo pip3 install pyaudio
+sudo pip3 install webdriver-manager
+
+sudo sudo apt-get install python3-pyqt4.qtopengl
+sudo apt install python3-pyaudio
+sudo sudo apt install linuxbrew-wrapper
+
+
+# echo -n -e ""$red"["$blue"RE4P"$yellow"@"$white"EUPERLINE"$red"]-["$yellow"~"$red"]"$transparent"" &
